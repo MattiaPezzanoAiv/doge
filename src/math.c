b474d7bf92216e73168cd1e42352c1b49cd944d4
Mattia <mattiapezzano7@gmail.com>
2017-11-09 12:46:57 +0100

    modified vector structure from heap alloc to stack alloc

--- src/math.c
+++ src/math.c
@@ -1,48 +1,45 @@
-#ifndef math_h
-#define math_h 
-#include "../include/math.h"
-#endif
 #include <stdlib.h>
 #include <math.h>
+#include "../include/math.h"
 
 //VECTOR 2
-struct doge_vec2* sum_vec2(struct doge_vec2* a,struct doge_vec2* b)
+struct doge_vec2 sum_vec2(struct doge_vec2 a,struct doge_vec2 b)
 {
-    struct doge_vec2* v = malloc(sizeof(struct doge_vec2));
-    v->x = a->x+b->x;
-    v->y = a->y+b->y;
+    struct doge_vec2 v;
+    v.x = a.x+b.x;
+    v.y = a.y+b.y;
     return v;
 }
-struct doge_vec2* sub_vec2(struct doge_vec2* a,struct doge_vec2* b)
+struct doge_vec2 sub_vec2(struct doge_vec2 a,struct doge_vec2 b)
 {
-    struct doge_vec2* v = malloc(sizeof(struct doge_vec2));
-    v->x = a->x-b->x;
-    v->y = a->y-b->y;
+    struct doge_vec2 v;
+    v.x = a.x-b.x;
+    v.y = a.y-b.y;
     return v;
 }
-struct doge_vec2* scale_vec2(struct doge_vec2* a, struct doge_vec2* b)
+struct doge_vec2 scale_vec2(struct doge_vec2 a, struct doge_vec2 b)
 {
-    struct doge_vec2* v = malloc(sizeof(struct doge_vec2));
-    v->x = a->x*b->x;
-    v->y = a->y*b->y;
+    struct doge_vec2 v;
+    v.x = a.x*b.x;
+    v.y = a.y*b.y;
     return v;
 }
-float slow_len_vec2(struct  doge_vec2* a)
+float slow_len_vec2(struct  doge_vec2 a)
 {
-   return (float)sqrt(pow(a->x,2)+pow(a->y,2));
+   return (float)sqrt(pow(a.x,2)+pow(a.y,2));
 }
-float fast_len_vec2(struct doge_vec2* a)
+float fast_len_vec2(struct doge_vec2 a)
 {
-   return (pow(a->x,2)+pow(a->y,2));
+   return (pow(a.x,2)+pow(a.y,2));
 }
-float distance_vec2(struct doge_vec2* a,struct doge_vec2* b)
+float distance_vec2(struct doge_vec2 a,struct doge_vec2 b)
 {
-    struct doge_vec2* c = sub_vec2(a,b);
+    struct doge_vec2 c = sub_vec2(a,b);
     return slow_len_vec2(c);
 }
 int normalize_vec2(struct doge_vec2* a)
 {
-    float a_len = slow_len_vec2(a);
+    float a_len = slow_len_vec2(*a);
     a->x = a->x / a_len;
     a->y = a->y / a_len;
     return 0;
@@ -51,46 +48,46 @@ int normalize_vec2(struct doge_vec2* a)
 
 
 //VECTOR 3
-struct doge_vec3* sum_vec3(struct doge_vec3* a, struct doge_vec3* b)
+struct doge_vec3 sum_vec3(struct doge_vec3 a, struct doge_vec3 b)
 {
-    struct doge_vec3* v = malloc(sizeof(struct doge_vec3));
-    v->x = a->x+b->x;
-    v->y = a->y+b->y;
-    v->z = a->z+b->z;
+    struct doge_vec3 v;
+    v.x = a.x+b.x;
+    v.y = a.y+b.y;
+    v.z = a.z+b.z;
     return v;
 }
-struct doge_vec3* sub_vec3(struct doge_vec3* a, struct doge_vec3* b)
+struct doge_vec3 sub_vec3(struct doge_vec3 a, struct doge_vec3 b)
 {
-    struct doge_vec3* v = malloc(sizeof(struct doge_vec2));
-    v->x = a->x-b->x;
-    v->y = a->y-b->y;
-    v->z = a->z-b->z;
+    struct doge_vec3 v;
+    v.x = a.x-b.x;
+    v.y = a.y-b.y;
+    v.z = a.z-b.z;
     return v;
 }
-struct doge_vec3* scale_vec3(struct doge_vec3* a, struct doge_vec3* b)
+struct doge_vec3 scale_vec3(struct doge_vec3 a, struct doge_vec3 b)
 {
-    struct doge_vec3* v = malloc(sizeof(struct doge_vec3));
-    v->x = a->x*b->x;
-    v->y = a->y*b->y;
-    v->z = a->z*b->z;
+    struct doge_vec3 v;
+    v.x = a.x*b.x;
+    v.y = a.y*b.y;
+    v.z = a.z*b.z;
     return v;
 }
-float slow_len_vec3(struct  doge_vec3* a)
+float slow_len_vec3(struct  doge_vec3 a)
 {
-   return (float)sqrt(pow(a->x,2)+pow(a->y,2)+pow(a->z,2));
+   return (float)sqrt(pow(a.x,2)+pow(a.y,2)+pow(a.z,2));
 }
-float fast_len_vec3(struct doge_vec3* a)
+float fast_len_vec3(struct doge_vec3 a)
 {
-   return (pow(a->x,2)+pow(a->y,2)+pow(a->z,2));
+   return (pow(a.x,2)+pow(a.y,2)+pow(a.z,2));
 }
-float distance_vec3(struct doge_vec3* a, struct doge_vec3* b)
+float distance_vec3(struct doge_vec3 a, struct doge_vec3 b)
 {
-    struct doge_vec3* c = sub_vec3(a,b);
+    struct doge_vec3 c = sub_vec3(a,b);
     return slow_len_vec3(c);
 }
 int normalize_vec3(struct doge_vec3* a)
 {
-    float a_len = slow_len_vec3(a);
+    float a_len = slow_len_vec3(*a);
     a->x = a->x / a_len;
     a->y = a->y / a_len;
     a->z = a->z / a_len;
--- src/test.c
+++ src/test.c
@@ -3,7 +3,7 @@
 // #include "../include/actor.h"
 #include "../include/test.h"
 
-#include "../include/math.h"
+#include "math.c"
 
 #include <stdlib.h>
 #include <string.h>
@@ -13,107 +13,98 @@
 //VECTOR 2
 int test_sum_vec2() //ok
 {
-    struct doge_vec2* a = malloc(sizeof(struct doge_vec2));
-	a->x = 10;
-	a->y = 10;
-	struct doge_vec2* b = malloc(sizeof(struct doge_vec2));
-	b->x = 10;
-	b->y = 10;
+    struct doge_vec2 a;
+	a.x = 10;
+	a.y = 10;
+	struct doge_vec2 b;
+	b.x = 10;
+	b.y = 10;
 	
-	struct doge_vec2* result = sum_vec2(a,b);
-    fprintf(stdout,"%f    %f  \n",result->x,result->y);
+	struct doge_vec2 result = sum_vec2(a,b);
+    fprintf(stdout,"%f    %f  \n",result.x,result.y);
     int ret = 1;
-    if(result->x == 20.0 && result->y == 20.0)
+    if(result.x == 20.0 && result.y == 20.0)
         ret = 0;
-
-        free(result);
     return ret;
 }
 int test_sub_vec2() //ok
 {
-    struct doge_vec2* a = malloc(sizeof(struct doge_vec2));
-	a->x = 15;
-	a->y = 10;
-	struct doge_vec2* b = malloc(sizeof(struct doge_vec2));
-	b->x = 30;
-	b->y = 5;
+    struct doge_vec2 a;
+	a.x = 15;
+	a.y = 10;
+	struct doge_vec2 b;
+	b.x = 30;
+	b.y = 5;
 	
-	struct doge_vec2* result = sub_vec2(a,b);
-    fprintf(stdout,"%f    %f  \n",result->x,result->y);
+	struct doge_vec2 result = sub_vec2(a,b);
+    fprintf(stdout,"%f    %f  \n",result.x,result.y);
     int ret = 1;
-    if(result->x == -15.0 && result->y == 5.0)
+    if(result.x == -15.0 && result.y == 5.0)
         ret = 0;
 
-        free(result);
     return  ret;
 }
 int test_scale_vec2() //ok
 {
-    struct doge_vec2* a = malloc(sizeof(struct doge_vec2));
-	a->x = 5;
-	a->y = 2;
-	struct doge_vec2* b = malloc(sizeof(struct doge_vec2));
-	b->x = 3;
-	b->y = 9;
+    struct doge_vec2 a;
+	a.x = 5;
+	a.y = 2;
+	struct doge_vec2 b;
+	b.x = 3;
+	b.y = 9;
 	
-	struct doge_vec2* result = scale_vec2(a,b);
+	struct doge_vec2 result = scale_vec2(a,b);
     int ret = 1;
-    if(result->x == 15 && result->y == 18)
+    if(result.x == 15 && result.y == 18)
         ret = 0;
 
-        free(result);
     return ret;
 }
 int test_slow_len_vec2() //ok
 {
-    struct doge_vec2* a = malloc(sizeof(struct doge_vec2));
-    a->x = 10;
-    a->y = 6;
+    struct doge_vec2 a;
+    a.x = 10;
+    a.y = 6;
     int ret = 1;
     float len = slow_len_vec2(a);
-    fprintf(stdout,"%f \n",len);
     if(len <= 11.662 && len >= 11.660) //error margin
         ret =    0;
-    free(a);
     return ret;
 }
 int test_fast_len_vec2() //ok
 {
-    struct doge_vec2* a = malloc(sizeof(struct doge_vec2));
-    a->x = 10;
-    a->y = 6;
+    struct doge_vec2 a;
+    a.x = 10;
+    a.y = 6;
     int ret = 1;
     float len = fast_len_vec2(a);
     if(len <= 136.001 && len >= 135.999) //error margin
         ret =    0;
-    free(a);
     return ret;
 }
 int test_distance_vec2() //ok
 {
-    struct doge_vec2* a = malloc(sizeof(struct doge_vec2));
-    a->x = 18;
-    a->y = 24;
-    struct doge_vec2* b = malloc(sizeof(struct doge_vec2));
-    b->x = -5;
-    b->y = -87;
+    struct doge_vec2 a;
+    a.x = 18;
+    a.y = 24;
+    struct doge_vec2 b;
+    b.x = -5;
+    b.y = -87;
     int ret = 1;
     float dist = distance_vec2(a,b); //113.357
     if(dist >= 113.356 && dist <= 113.358)
         ret = 0;
-    free(a);free(b);
     return ret;
 }
 int test_normalize_vec2()
 {
-    struct doge_vec2* a = malloc(sizeof(struct doge_vec2));
-    a->x = 10;
-    a->y = 6;
+    struct doge_vec2 a;
+    a.x = 10;
+    a.y = 6;
     int ret = 1;
-    normalize_vec2(a); //expected x = 0.857 y = 0.514
-    if(a->x >= 0.856 && a->x <= 0.858 && a->y >= 0.513 && a->y <= 0.515)
+    normalize_vec2(&a); //expected x = 0.857 y = 0.514
+    if(a.x >= 0.856 && a.x <= 0.858 && a.y >= 0.513 && a.y <= 0.515)
         ret = 0;
-    free(a);
     return ret;
 }
 
@@ -124,139 +115,135 @@ int test_normalize_vec2()
 //VECTOR 3
 int test_sum_vec3() //ok
 {
-    struct doge_vec3* a = malloc(sizeof(struct doge_vec3));
-	a->x = 10;
-    a->y = 10;
-    a->z = 30;
-	struct doge_vec3* b = malloc(sizeof(struct doge_vec3));
-	b->x = 10;
-	b->y = 10;
-	b->z = 9;
-	struct doge_vec3* result = sum_vec3(a,b);
+    struct doge_vec3 a;
+	a.x = 10;
+    a.y = 10;
+    a.z = 30;
+	struct doge_vec3 b;
+	b.x = 10;
+	b.y = 10;
+	b.z = 9;
+	struct doge_vec3 result = sum_vec3(a,b);
     int ret = 1;
-    if(result->x == 20.0 && result->y == 20.0 && result->z == 39)
+    if(result.x == 20.0 && result.y == 20.0 && result.z == 39)
         ret = 0;
 
-        free(result);
     return ret;
 }
 int test_sub_vec3() //ok
 {
-    struct doge_vec3* a = malloc(sizeof(struct doge_vec3));
-	a->x = 15;
-    a->y = 10;
-    a->z = 5;
-	struct doge_vec3* b = malloc(sizeof(struct doge_vec3));
-	b->x = 30;
-	b->y = 5;
-	b->z = 5;
-	struct doge_vec3* result = sub_vec3(a,b);
+    struct doge_vec3 a;
+	a.x = 15;
+    a.y = 10;
+    a.z = 5;
+	struct doge_vec3 b;
+	b.x = 30;
+	b.y = 5;
+	b.z = 5;
+	struct doge_vec3 result = sub_vec3(a,b);
     int ret = 1;
-    if(result->x == -15.0 && result->y == 5.0 && result->z == 0)
+    if(result.x == -15.0 && result.y == 5.0 && result.z == 0)
         ret = 0;
 
-        free(result);
     return  ret;
 }
 int test_scale_vec3() //ok
 {
-    struct doge_vec3* a = malloc(sizeof(struct doge_vec3));
-	a->x = 5;
-    a->y = 2;
-    a->z = 10;
-	struct doge_vec3* b = malloc(sizeof(struct doge_vec3));
-	b->x = 3;
-	b->y = 9;
-    b->z = 10;
+    struct doge_vec3 a;
+	a.x = 5;
+    a.y = 2;
+    a.z = 10;
+	struct doge_vec3 b;
+	b.x = 3;
+	b.y = 9;
+    b.z = 10;
     
-	struct doge_vec3* result = scale_vec3(a,b);
+	struct doge_vec3 result = scale_vec3(a,b);
     int ret = 1;
-    if(result->x == 15 && result->y == 18 && result->z == 100)
+    if(result.x == 15 && result.y == 18 && result.z == 100)
         ret = 0;
 
-        free(result);
     return ret;
 }
 int test_slow_len_vec3() //ok
 {
-    struct doge_vec3* a = malloc(sizeof(struct doge_vec3));
-    a->x = 10;
-    a->y = 6;
-    a->z = 45;
+    struct doge_vec3 a;
+    a.x = 10;
+    a.y = 6;
+    a.z = 45;
     int ret = 1;
     float len = slow_len_vec3(a);
     if(len <= 46.487 && len >= 46.485) //error margin
         ret =    0;
-    free(a);
     return ret;
 }
 int test_fast_len_vec3() //ok
 {
-    struct doge_vec3* a = malloc(sizeof(struct doge_vec3));
-    a->x = 10;
-    a->y = 6;
-    a->z = 45;
+    struct doge_vec3 a;
+    a.x = 10;
+    a.y = 6;
+    a.z = 45;
     int ret = 1;
     float len = fast_len_vec3(a);
     if(len <= 2161.001 && len >= 2160.999) //error margin
         ret =    0;
-    free(a);
     return ret;
 }
 int test_distance_vec3()
 {
-    struct doge_vec3* a = malloc(sizeof(struct doge_vec3));
-    a->x = 18;
-    a->y = 24;
-    a->z = 9;
-    struct doge_vec3* b = malloc(sizeof(struct doge_vec3));
-    b->x = -5;
-    b->y = -87;
-    b->z = 11;
+    struct doge_vec3 a;
+    a.x = 18;
+    a.y = 24;
+    a.z = 9;
+    struct doge_vec3 b;
+    b.x = -5;
+    b.y = -87;
+    b.z = 11;
     int ret = 1;
     float dist = distance_vec3(a,b); //113.375
     if(dist >= 113.374 && dist <= 113.376)
         ret = 0;
-    free(a);free(b);
     return ret;
 }
 int test_normalize_vec3()
 {
-    struct doge_vec3* a = malloc(sizeof(struct doge_vec3));
-    a->x = 10;
-    a->y = 6;
-    a->z = 45;
+    struct doge_vec3 a;
+    a.x = 10;
+    a.y = 6;
+    a.z = 45;
     int ret = 1;
-    normalize_vec3(a); //expected x = 0.215 y = 0.129 z = 0.968
-    if(a->x >= 0.214 && a->x <= 0.216 && a->y >= 0.128 && a->y <= 0.130 && a->z >= 0.967 && a->z <= 0.969)
+    normalize_vec3(&a); //expected x = 0.215 y = 0.129 z = 0.968
+    if(a.x >= 0.214 && a.x <= 0.216 && a.y >= 0.128 && a.y <= 0.130 && a.z >= 0.967 && a.z <= 0.969)
         ret = 0;
-    free(a);
     return ret;
 }
 
+
+//call dlsym
+//
 
 //TRANSFORM
-int test_transform_init2()
-{
-    struct doge_transform* t = malloc(sizeof(struct doge_transform));
-    struct doge_vec2* pippo = malloc(sizeof(struct doge_vec2));
-    pippo->x = 100;pippo->y=100;
-    struct doge_vec2* pippo2 = malloc(sizeof(struct doge_vec2));
-    pippo2->x = 25;
+// int test_transform_init2()
+// {
+//     struct doge_transform* t = malloc(sizeof(struct doge_transform));
+//     struct doge_vec2* pippo = malloc(sizeof(struct doge_vec2));
+//     pippo->x = 100;pippo->y=100;
+//     struct doge_vec2* pippo2 = malloc(sizeof(struct doge_vec2));
+//     pippo2->x = 25;
     
-    transform_init_2(t,*pippo,*pippo2);
-    int ret = 0;
-    if(t->location.vec2.x != 100.00 || t->scale.vec2.x != 25.00)
-        ret = -1;
-    return ret;
-}
-int test_new_transform()
-{
-    struct doge_transform* t = new_transform();
-    if(!t)
-        return -1;
-    return 0;
-}
+//     transform_init_2(t,*pippo,*pippo2);
+//     int ret = 0;
+//     if(t->location.vec2.x != 100.00 || t->scale.vec2.x != 25.00)
+//         ret = -1;
+//     return ret;
+// }
+// int test_new_transform()
+// {
+//     struct doge_transform* t = new_transform();
+//     if(!t)
+//         return -1;
+//     return 0;
+// }
 
 //ACTOR
 // int test_new_actor_name_default()
@@ -276,8 +263,8 @@ int main(int argsCount, char* args[])
     // test(test_new_actor_name_default);
 
     //transform
-    test_transform_init2();
-    test_new_transform();
+    // test_transform_init2();
+    // test_new_transform();
 
 
     //vector2
